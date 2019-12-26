8ddf84fa167b8a0ed1a279e0a9132158e01734fd
Lim Jeong Muk <jmlim@neowiz.com>
2019-12-26 18:22:07 +0900

    소셜 로그인 네이버 추가

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 ### freelec 스프링 부트로 구현하는 웹 서비스 책 따라해보기
 
 - boot 와 JUnit 만 최신껄로 올림.
-- 와이프 가르쳐주기 용도.
+- 스프링 부트 와이프 가르쳐주기 용도로 딱인듯.
+
+### application-oauth.properties 
+
+~~~properties
+## google
+spring.security.oauth2.client.registration.google.client-id=클라이언트 ID
+spring.security.oauth2.client.registration.google.client-secret=시크릿키
+spring.security.oauth2.client.registration.google.scope=profile,email
+
+
+## naver registration
+spring.security.oauth2.client.registration.naver.client-id=클라이언트 ID
+spring.security.oauth2.client.registration.naver.client-secret=시크릿키
+spring.security.oauth2.client.registration.naver.redirect-uri={baseUrl}/{action}/oauth2/code/{registrationId}
+spring.security.oauth2.client.registration.naver.authorization-grant-type=authorization_code
+spring.security.oauth2.client.registration.naver.scope=name,email,profile_image
+spring.security.oauth2.client.registration.naver.client-name=Naver
+
+## naver provider
+spring.security.oauth2.client.provider.naver.authorization_uri=https://nid.naver.com/oauth2.0/authorize
+spring.security.oauth2.client.provider.naver.token_uri=https://nid.naver.com/oauth2.0/token
+spring.security.oauth2.client.provider.naver.user-info-uri=https://openapi.naver.com/v1/nid/me
+
+### 기준이 되는 user_name의 이름을 네이버에서는 response로 해야 함. 네이버의 회원 조회 시 반환되는 JSON 형태 때문
+spring.security.oauth2.client.provider.naver.user_name_attribute=response
+~~~
--- src/main/java/io/jmlim/freelec/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/io/jmlim/freelec/springboot/config/auth/dto/OAuthAttributes.java
@@ -4,10 +4,12 @@ import io.jmlim.freelec.springboot.domain.user.Role;
 import io.jmlim.freelec.springboot.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.Objects;
 
+@Slf4j
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
@@ -28,15 +30,33 @@ public class OAuthAttributes {
 
     //
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if (registrationId.equals("naver")) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        log.info(" ---> google attributes : {}", attributes);
+
         return OAuthAttributes.builder()
                 .name(Objects.toString(attributes.get("name")))
                 .email(Objects.toString(attributes.get("email")))
                 .picture(Objects.toString(attributes.get("picture")))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        log.info(" ---> naver response : {}, attributes: {}", response, attributes);
+        return OAuthAttributes.builder()
+                .name(Objects.toString(response.get("name")))
+                .email(Objects.toString(response.get("email")))
+                .picture(Objects.toString(response.get("profileImage")))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
