a9051bb32f8003bc4f9151137d3109779d3af284
Lim Jeong Muk <jmlim@neowiz.com>
2019-12-26 17:42:23 +0900

    소셜 로그인 개선, session 저장소 추가(jdbc)

--- src/main/java/io/jmlim/freelec/springboot/web/IndexController.java
+++ src/main/java/io/jmlim/freelec/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package io.jmlim.freelec.springboot.web;
 
+import io.jmlim.freelec.springboot.config.auth.LoginUser;
 import io.jmlim.freelec.springboot.config.auth.dto.SessionUser;
 import io.jmlim.freelec.springboot.service.posts.PostsService;
 import io.jmlim.freelec.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (Objects.nonNull(user)) {
             model.addAttribute("userName", user.getName());
